d97a47547446a6cf06047a0398b0137b2b2c73af
Duraj-vut <257095@vutbr.cz>
2025-06-15 18:10:03 +0200

    uprava referencii

--- main.js
+++ main.js
@@ -54,3 +54,58 @@ function showDefault() {
 
   currentSection = "default";
 }
+
+
+//Slider
+const sliderStates = {
+  instalaterskeSluzby: { index: 0 },
+  topenarskeSluzby: { index: 0 },
+  stehovani: { index: 0 },
+  odtahy: { index: 0 },
+  tepovani: { index: 0 },
+  dezinfekce: { index: 0 }
+};
+
+const sliderIds = ['instalaterskeSluzby', 'topenarskeSluzby', 'stehovani', 'odtahy', 'tepovani', 'dezinfekce'];
+
+function slide(sliderId, direction) {
+  const container = document.getElementById(`slider-${sliderId}`);
+  const track = container.querySelector(".slider-track");
+  const images = track.querySelectorAll("img");
+  const prevBtn = container.querySelector(".slider-button.prev");
+  const nextBtn = container.querySelector(".slider-button.next");
+
+  if (images.length === 0) return; 
+
+  const visibleCount = Math.floor(container.clientWidth / images[0].clientWidth);
+  const maxIndex = images.length - visibleCount;
+
+  const totalImagesWidth = images.length * (images[0].clientWidth + 12);
+
+  if (totalImagesWidth <= container.clientWidth) {
+    prevBtn.style.display = "none";
+    nextBtn.style.display = "none";
+  } else {
+    prevBtn.style.display = "block";
+    nextBtn.style.display = "block";
+  }
+
+  sliderStates[sliderId].index += direction;
+
+  if (sliderStates[sliderId].index < 0) {
+    sliderStates[sliderId].index = 0;
+  } else if (sliderStates[sliderId].index > maxIndex) {
+    sliderStates[sliderId].index = maxIndex;
+  }
+
+  const shift = -(images[0].clientWidth + 12) * sliderStates[sliderId].index;
+  track.style.transform = `translateX(${shift}px)`;
+}
+
+sliderIds.forEach(id => slide(id, 0));
+
+window.addEventListener('resize', () => {
+  sliderIds.forEach(id => slide(id, 0));
+});
+
+
